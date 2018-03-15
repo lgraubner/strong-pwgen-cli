479a426f0763b15ad6f00a008f193cd02b3a934e
Lars Graubner <mail@larsgraubner.de>
2018-03-15 23:57:23 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Strong password generator CLI
+
+[![Travis](https://img.shields.io/travis/lgraubner/strong-pwgen-cli.svg)](https://travis-ci.org/lgraubner/strong-pwgen-cli) [![David](https://img.shields.io/david/lgraubner/strong-pwgen-cli.svg)](https://david-dm.org/lgraubner/strong-pwgen-cli) [![npm](https://img.shields.io/npm/v/strong-pwgen-cli.svg)](https://www.npmjs.com/package/strong-pwgen-cli)
+
+> Generate cryptographically strong passwords.
+
+@TODO
+
+## Table of contents
+
+- [Install](#install)
+- [Usage](#usage)
+- [Options](#options)
+- [License](#license)
+
+## Install
+
+This module is available on [npm](https://www.npmjs.com/).
+
+```BASH
+npm install -g strong-pwgen-cli
+# or execute it directly with npx (since npm v5.2)
+npx strong-pwgen-cli
+```
+
+## Usage
+
+
+```BASH
+pwgen [options]
+```
+
+## Options
+
+### length
+
+Adjust the length of the password.
+
+```
+pwgen --length 20
+# or
+pwgen -l 20
+```
---
+++ index.js
@@ -0,0 +1,62 @@
+const generate = require('nanoid/generate');
+const clipboardy = require('clipboardy');
+const mri = require('mri');
+
+const chars =
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !"#$%&\'()*+,-./:;<=>?@[]^_`{|}~';
+
+const help = () =>
+  console.log(
+    `
+  Usage: pwgen [options]
+
+  Options:
+
+    -h, --help      output usage information
+    -V, --version   output the version number
+    -l, --length    length of password (32)
+  `
+  );
+
+const main = argv_ => {
+  const argv = mri(argv_, {
+    alias: {
+      l: 'length',
+      h: 'help',
+      v: 'version'
+    },
+    default: {
+      length: 32
+    },
+    boolean: ['help', 'version']
+  });
+
+  // print version
+  if (argv.version) {
+    console.log(require('./package').version);
+    return 0;
+  }
+
+  // show help
+  if (argv.help) {
+    help();
+    return 0;
+  }
+
+  // show warning for passwords < 10
+  if (argv.length < 10) {
+    console.log(
+      '⚠️  Passwords with less than ten characters are not secure. (https://goo.gl/SzkLui)'
+    );
+    return 0;
+  }
+
+  // generate password and copy to clipboard
+  const pass = generate(chars, argv.length);
+  clipboardy.writeSync(pass);
+  console.log('📋  copied to clipboard');
+
+  process.exit();
+};
+
+main(process.argv);
